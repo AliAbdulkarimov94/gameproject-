27df0444a2880c3468ea8acb851a57858cfdabb6
AliAbdulkarimov94 <alikenbo@gmail.com>
2021-10-11 19:49:48 +0300

    the gussing game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [started, setStarted] = useState(false);
+  const [status, setStatus] = useState(false);
+  const [answer, setAnswer] = useState(0);
+  const [rightAnswer, setRightAnsweer] = useState(0);
+  console.log(rightAnswer);
+
+  const submit = (e) => {
+    e.preventDefault();
+    const formValid = +answer >= 0;
+    if (!formValid) {
+      return;
+    }
+    if (+answer === +rightAnswer) {
+      setStatus("you got it");
+      setStarted(false);
+    } else if (+answer < +rightAnswer) {
+      setStatus("too low");
+    } else {
+      setStatus("too high");
+    }
+  };
+
+  const start = () => {
+    setRightAnsweer(Math.ceil(Math.random() * 120));
+    setStarted(true);
+  };
+
+  if (started) {
+    return (
+      <div>
+        <form onSubmit={submit}>
+          <div>
+            <label>answer</label>
+            <input value={answer} onChange={(e) => setAnswer(e.target.value)} />
+          </div>
+          <button type="submit">check</button>
+        </form>
+        <p>{status}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <button type="button" onClick={start}>
+          start
+        </button>
+        <p>{status}</p>
+      </div>
+    );
+  }
 }
-
-export default App;
